2ae4360b63cc0821f6323ee8a7ccfa5454048bad
XenoShot <fusionbomb2004@gmail.com>
2024-06-07 00:05:01 +0530

    refactored split and embedd function

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import BedrockEmbeddings
 import os
 
+import nest_asyncio
+nest_asyncio.apply()
+
 embeddings = BedrockEmbeddings(
     region_name="us-west-2"
 )
@@ -27,24 +30,25 @@ def document_splitter(document):
     chunks = text_splitter.split_documents([document])
     return chunks
 
-def embed_files(file_path_list):
-    document_list = []
-    
-    for document in file_path_list:
-        loader = PyPDFLoader(document)
-        #document split into list of pages
-        document = loader.load()
-        pages = []
-        
-        for page in document:
-            page_chunks = document_splitter(page)
-            pages.append(page_chunks)
-            embeddings.aembed_documents(page_chunks)
+def split_and_embed_document(document):
+    for page in document:
+        page_chunks = document_splitter(page)
+        embeddings.aembed_documents(page_chunks) 
 
-        document_list.append(pages)
 
-    return document_list
+def load_file(file_path):
+    loader = PyPDFLoader(file_path)
+    document = loader.load()
+    return document
+
 
 file_path_list = return_filelist("documents")
-document_list = embed_files(file_path_list)
+
+
+for file_path in file_path_list:
+    document = load_file(file_path)
+    split_and_embed_document(document)
+
+
+
 
