ac74664bb56f68cfcb5f762599cadb40e6a7440f
XenoShot <fusionbomb2004@gmail.com>
2024-06-19 19:03:31 +0530

    impliment llm model to chatbot

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import BedrockEmbeddings
+from langchain_aws import ChatBedrock
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
 import boto3
@@ -29,6 +33,20 @@ embeddings = BedrockEmbeddings(
     region_name="us-west-2"
 )
 
+params = {
+    "temperature": 0.2,
+    "top_k": 250,
+    "top_p": 1,
+    "stop_sequences": ["\n\nHuman"]
+}
+
+llm = ChatBedrock(
+    client= bedrock_runtime_client,
+    model_id="anthropic.claude-v2",
+    model_kwargs=params
+)
+
+
 database = Chroma(embedding_function=embeddings, persist_directory="./database")
 
 def return_filelist(path : str) -> list:
@@ -42,8 +60,8 @@ def return_filelist(path : str) -> list:
 
 def document_splitter(document):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 500,
-        chunk_overlap = 100,
+        chunk_size = 2000,
+        chunk_overlap = 400,
         length_function = len,
         is_separator_regex= False
     )
@@ -57,6 +75,7 @@ def split_and_embed_document(document):
         if(len(page_chunks) == 0):
             continue
         page_chunks_id = database.add_documents(page_chunks)
+        print(page_chunks_id)
 
 
 def load_file(file_path):
@@ -95,18 +114,31 @@ async def main():
         
         documents = load_file(file_path)
         split_and_embed_document(documents)
-        print(f"\n{file_path} added to the database")
+        print(f"\n{file_path} added to the database")  
     
-
+    retriever = database.as_retriever(search_type="mmr")
+    system_prompt = (
+    "Use the given context to answer the question. "
+    "If you don't know the answer, say you don't know. "
+    "keep the answer concise. "
+    "Context: {context}"
+    )
+    prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm, prompt)
+    chain = create_retrieval_chain(retriever, question_answer_chain)
+    
     while True:
-        query = input("\nEnter a query to search through the files(x to exit) : \n")
-        
+        query = input("Enter the question(x to exit) : ")
         if(query.upper() == "X"):
             break
         
-        retriever = database.as_retriever(search_type="mmr")
-        result = retriever.invoke(query)
-        print(f"\n{result[0].page_content}")
+        print(chain.invoke({"input":query})["answer"])
+
 
         
 
